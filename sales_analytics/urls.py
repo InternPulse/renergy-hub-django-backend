7759216b79010d75b811f3242ab604d56d7c91bb
Pere <yodas.elite@gmail.com>
2024-11-15 21:16:39 +0100

    First API Look - Sales Analytics

--- sales_analytics/urls.py
+++ sales_analytics/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from .api.v1 import views
 
 urlpatterns = [
-    
+    path('', views.SalesPerformanceView.as_view(), name='sales-performance'),
 ]
