4f58c97a5154b9859dde6637e861f8267ca80d92
Pere <yodas.elite@gmail.com>
2024-11-22 14:25:13 +0100

    Fixed API Structure - Added status message to API. Added tests

--- sales_analytics/api/v1/serializers.py
+++ sales_analytics/api/v1/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
 class SalesPerformanceSerializer(serializers.Serializer):
+    status = serializers.CharField(default="success")
+    message = serializers.CharField(default="Data retrieved successfully")
     revenue_trends = serializers.DictField()
     sales_by_product = serializers.DictField()
     # sales_by_region = serializers.DictField()
